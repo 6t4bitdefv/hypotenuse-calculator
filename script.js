a8f920f5aa3d50099a722725afdadf0d112afbd4
6t4bitdefv <strictly4github@gmail.com>
2023-01-03 16:23:12 +0100

    Uncommented index.js file

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// let a;
-// let b;
-// let c;
+let a;
+let b;
+let c;
 
-// document.getElementById("submitButton").onclick = function () {
-//   a = document.getElementById("aTextBox").value;
-//   a = Number(a);
+document.getElementById("submitButton").onclick = function () {
+  a = document.getElementById("aTextBox").value;
+  a = Number(a);
 
-//   b = document.getElementById("bTextBox").value;
-//   b = Number(b);
+  b = document.getElementById("bTextBox").value;
+  b = Number(b);
 
-//   c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+  c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
-//   document.getElementById("cLabel").textContent = "Side C: " + c;
-// };
+  document.getElementById("cLabel").textContent = "Side C: " + c;
+};
